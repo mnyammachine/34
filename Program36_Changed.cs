a1abbfd7134051277d4e9129c2777e488675289e
Ksenia Marchenko <106731538+mnyammachine@users.noreply.github.com>
2023-02-16 14:09:36 +0200

    Add files via upload

---
+++ Program36_Changed.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+// Длинна массива вводится из терминала 
+Console.Write($"Введите длину массива, указав количество его элементов: ");
+int numberElements = Convert.ToInt32(Console.ReadLine()); 
+// Заполнение массива случайными числами
+int RandomNumbers(int numberElements, int min, int max)
+  {
+  int[] randomNumbers = new int[numberElements];
+  int sumElements = 0;
+  Console.Write("Массив: ");
+// Поиск суммы элементов, стоящих на нечётных позициях  
+    for (int i = 0; i <randomNumbers.Length; i++ ){
+      randomNumbers[i] = new Random().Next(min, max);
+
+      Console.Write(randomNumbers[i] + " ");
+
+      if (i % 2 != 0)
+      {
+        sumElements = sumElements + randomNumbers[i];
+      }
+    }
+  return sumElements;
+  }
+
+int randomNumbers =  RandomNumbers(numberElements, -100, 100);
+
+Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
+
+
