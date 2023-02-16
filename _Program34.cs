7c30c16bde7cc7146ecb2d0cbdb361c27fdbbe5c
Ksenia Marchenko <106731538+mnyammachine@users.noreply.github.com>
2023-02-16 14:14:18 +0200

    Add files via upload

---
+++ _Program34.cs
@@ -0,0 +1,47 @@
+﻿
+
+// задать массив
+int[] array = new int[8];
+// заполнить массив случайными положительными трехзначными числами 
+FillArray (array, 100, 999);
+// показать количество четных чисел в массиве
+Console.WriteLine ("Количество положительных элементов в массиве");
+
+// заполняю массив 
+void FillArray(int[] arr, int minVal, int maxVal)
+{
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++ ) {
+        arr[i] = rnd.Next(minVal, maxVal + 1);
+    }
+}
+
+void PrintArray(int[] array)
+{
+  for (int i = 0; i<array.Length; i++ )
+  {
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
+
+int QuantityPositive(int[] array)
+{
+    int quantity = 0;
+    for (int i = 0; i<array.Length; i++ )
+    {
+    if (array[i] % 2 == 0)
+     {
+      quantity++;
+     }
+    }
+  return quantity;
+}
+
+FillArray(array, 100, 1000);
+PrintArray(array);
+Console.WriteLine();
+
+int quantity = QuantityPositive(array);
+Console.WriteLine($"Количество чётных чисел в массиве: {quantity}");
